2f73ff06b775cab086d9519af50413b83d8a3822
joaovictor307 <163419377+joaovictor307@users.noreply.github.com>
2024-09-05 12:29:47 -0300

    Add files via upload

---
+++ js/oi.js
@@ -0,0 +1,8 @@
+let anonacimento, soma
+do {
+    anonacimento=prompt("informe a idade")
+} while (anonacimento>2024);{
+    soma= 2024- anonacimento
+
+    document.write(`voce tera ${2024} exatamente ${soma} anos em 2024`)
+}
